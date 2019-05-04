ecaa0af996fccd52d9ee8d0e5eced43dc7a4ee1d
shlroland <shlroland1995@gmail.com>
2019-05-05 00:11:34 +0800

    init&完成三个组件构建

---
+++ components/button/button.js
@@ -0,0 +1,18 @@
+component(
+    {
+        properties{
+            size: {
+                type: String,
+                value: ''
+              },
+              type: {
+                type: String,
+                value: "default"
+              },
+              text: {
+                type: String,
+                value: ''
+              }
+        }
+    }
+)
---
+++ components/clock/clock.js
@@ -0,0 +1,13 @@
+
+Component({
+    properties: {
+      text: {
+        type: String,
+        value: ""
+      },
+      fontSize: {
+        type: String,
+        value: "32"
+      }
+    }
+  })
