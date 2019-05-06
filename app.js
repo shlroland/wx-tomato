cc4fe593641302741c2517a61794c3c52a5347de
shlroland <shlroland1995@gmail.com>
2019-05-06 23:43:56 +0800

    删除app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-      host: 'https://gp-server.hunger-valley.com',
-      app_id: "wxa206ae12a17fea24",
-      app_secret: "99b0313fbaee16684c14f1daa7116171",
-      t_app_id: "45fkG1DsQS45nWU7QMfHK11p",
-      t_app_secret: "45fkG1DsQS45nWU7QMfHK11p"
-  }
- })
