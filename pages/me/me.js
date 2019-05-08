98d8b5a5bef2fca1dd774724012ee29b932114d9
shlroland <shlroland1995@gmail.com>
2019-05-08 23:59:44 +0800

    完成我的页面历史数据接入

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,25 +1,33 @@
 // pages/me/me.js
+const { http } = require('../../utils/http')
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  onShow(){
+    this.fetchTomatoes()
+    this.fetchTodos()
+    this.setData({me: wx.getStorageSync('me')})
+  },
   data: {
     tab: "tomato",
-    lists: {
-      "本周四": [
-        { time: "14:00", text: "阿含经开发你看看书法课", id: 1 },
-        { time: "14:00", text: "阿含经开发你看看书法课", id: 4 }
-      ],
-      "本周五": [{ time: "14:00", text: "阿含经开发你看看书法课", id: 2 }],
-      "本周天": [{ time: "14:00", text: "阿含经开发你看看书法课", id: 3 }],
-      "本周一": [{ time: "14:00", text: "阿含经开发你看看书法课", id: 3 }],
-      "本周二": [{ time: "14:00", text: "阿含经开发你看看书法课", id: 3 }],
-      "本周三": [{ time: "14:00", text: "阿含经开发你看看书法课", id: 3 }],
-    }
+    tomatoes:{},
+    todos:{},
+    me:{}
   },
   changeTab(e){
     let name = e.currentTarget.dataset.name
     this.setData({tab:name})
+  },
+  fetchTomatoes(){
+    http.get('/tomatoes',{is_group:'yes'})
+    .then(res=>{
+      
+      this.setData({tomatoes:res.data.resources})
+    })
+  },
+  fetchTodos(){
+    http.get('/todos',{is_group:'yes'})
+    .then(res=>{
+      console.log(res.data.resources)
+      this.setData({todos:res.data.resources})
+    })
   }
 })
