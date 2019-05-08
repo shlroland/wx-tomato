a43eb19b4457809fa663701b46fc052b2103dea1
shlroland <shlroland1995@gmail.com>
2019-05-08 21:18:46 +0800

    home页api绑定（api调用未正常）

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,39 +1,72 @@
+const { http } = require('../../utils/http')
 Page({
+  updateId: '',
+  updatIndex: '',
   data: {
     lists: [
-      {id: 1, text: "我今天干了什么，明天干什么，后天干了什么", finished: true},
-      { id: 2, text: "后天噶什么，几天干甚了饿是卡号发给看书看发 ", finished: true },
-      { id: 3, text: "我今天干了什么，明天干什么，后天干了什么", finished: false },
-      { id: 4, text: "我今天干了什么，明天干什么，后天干了什么", finished: false },
-      { id: 5, text: "我今天干了什么，明天干什么，后天干了什么", finished: false },
-      {id: 6, text: "我今天干了什么，明天干什么，后天干了什么", finished: true},
-      { id: 7, text: "后天噶什么，几天干甚了饿是卡号发给看书看发 ", finished: true },
-      { id: 8, text: "我今天干了什么，明天干什么，后天干了什么", finished: false },
-      { id: 9, text: "我今天干了什么，明天干什么，后天干了什么", finished: false },
+      {id: 1, description: "我今天干了什么，明天干什么，后天干了什么", conpleted: false},
     ],
-    visibleConfirm: false
+    visibleConfirm: false,
+    visibleUpdateConfirm: false,
+    updateContent: "",
   },
-  confirmCreate(event){
-    
+  onShow() {
+    http.get('/todos?completed=false').then(res => {
+      this.setData({ lists: res.data.resources })
+    })
+  },
+  confirmCreate(event) {
     let content = event.detail
-    if(content){
-      console.log(2)
-      let todo = [{id:this.data.lists.length + 1,text:content,finished:false}]
-      this.data.lists = todo.concat(this.data.lists)
-      this.setData({lists:this.data.lists})
-      this.hideConfirm()
+    console.log(content)
+    if (content) {
+      http.post('/todos', {
+        completed: false, description: content
+      }).then(res => {
+        console.log(res)
+        let todo = [res.data.resource]
+        console.log("todo")
+        console.log(todo)
+        this.data.lists = todo.concat(this.data.lists)
+        this.setData({ lists: this.data.lists })
+        this.hideConfirm()
+      })
     }
   },
-  destroyTodo(event){
-    
-    let index = event.currentTarget.dataset.index
-    this.data.lists[index].finished = true
-    this.setData({ lists: this.data.lists })
+  changeText(event) {
+    let { content, id, index } = event.currentTarget.dataset
+    this.updataId =id
+    this.updatIndex =index
+    this.setData({visibleUpdateConfirm:true,updateContent:content})
   },
-  hideConfirm(){
+  confirmUpdate(event){
+    let content = event.detail
+    http.put(`/todos/${this.updataId}`,{
+      description:content
+    }).then(res=>{
+      let todo = res.data.resource
+      this.data.lists[this.updatIndex] = todo
+      this.setData({lists:this.data.lists})
+      this.hideConfirm()
+    })
+  },
+  destroyTodo(event) {
+    let index = event.currentTarget.dataset.index
+    let id = event.currentTarget.dataset.id
+    http.put(`/todos/${id}`, {
+      conpleted: true
+    }).then(res => {
+      let todo = res.data.resource
+      this.data.lists[index] = todo
+      this.setData({ lists: this.data.lists })
+    })
+  },
+  hideConfirm() {
     this.setData({ visibleConfirm: false })
   },
-  showConfirm(){
+  showConfirm() {
     this.setData({ visibleConfirm: true })
+  },
+  hideUpdateConfirm() {
+    this.setData({ visibleUpdateConfirm: false })
   }
 })
