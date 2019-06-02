f586440841b48f53138e88a6b0e1e889c1e1c93d
shlroland <shlroland1995@gmail.com>
2019-06-03 00:03:58 +0800

    修复swiper

--- pages/me/me.js
+++ pages/me/me.js
@@ -11,20 +11,28 @@ Page({
     tomatoes:{},
     todos:{},
     me:{},
-    tabIndex:'0'
+    tabIndex:0
   },
-  changeTab(e){
-    let name = e.currentTarget.dataset.name
-    if(name === 'tomato'){
-      this.data.tabIndex = '0'
-    }else if(name === 'task'){
-      this.data.tabIndex = '1'
-    }
-    this.setData({tab:name})
-    this.setData({tabIndex : this.data.tabIndex })
+  currentChange(e) {
+    let current = e.detail.current
+    console.log(current)
+    this.setData({ tabIndex: current })
   },
-  currentChange(e){
-    this.setData({tabIndex : e.detail.current })
+  changeTomato(e){
+    // let current = parseFloat(e.currentTarget.dataset.current)
+    // if(this.data.tabIndex == current){
+    //   return false
+    // }else{
+      this.setData({tabIndex:0})
+    // }
+  },
+  changeTask(e) {
+    // let current = parseFloat(e.currentTarget.dataset.current)
+    // if (this.data.tabIndex == current) {
+    //   return false
+    // } else {
+      this.setData({tabIndex:1})
+    // }
   },
   fetchTomatoes(){
     http.get('/tomatoes',{is_group:'yes'})
@@ -35,7 +43,6 @@ Page({
   fetchTodos(){
     http.get('/todos',{is_group:'yes'})
     .then(res=>{
-      console.log(res)
       this.setData({todos:res.data.resources})
     })
   },
